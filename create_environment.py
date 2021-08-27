189b1411bc8989d4e535a99552d3ca159b0e22df
lewelyn7 <karol.hamielec@gmail.com>
2021-08-27 09:29:22 +0200

    create_enironment update

--- create_environment.py
+++ create_environment.py
@@ -1,4 +1,14 @@
 import boto3
-s3 = boto3.client('s3',region_name="localhost", endpoint_url="http://localhost:4566")
-s3.create_bucket(Bucket="crp-agh-settings")
-# s3.upload_file("DEVsettings.json", "crp-agh-settings", "candle_data_service")
+import botocore.exceptions
+import time
+done = False
+while not done:
+    try:
+        s3 = boto3.client('s3',region_name="localhost", endpoint_url="http://localhost:4566")
+        s3.create_bucket(Bucket="crp-agh-settings")
+        # s3.upload_file("DEVsettings.json", "crp-agh-settings", "candle_data_service")
+        print("created")
+        done = True
+    except botocore.exceptions.EndpointConnectionError as e:
+        print("FAIL... retry in 5s")
+        time.sleep(5)
